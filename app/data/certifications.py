d35b013a9882738ee8ed8308f168668aad51ec8a
DHRUV <141848452+22f3002319@users.noreply.github.com>
2026-01-09 22:23:49 +0530

    Create certifications.py with get_certifications function
    Added a function to return a list of certifications with details.

---
+++ app/data/certifications.py
@@ -0,0 +1,23 @@
+"""
+Certifications data.
+"""
+
+
+def get_certifications():
+    """Return list of certifications."""
+    return [
+        {
+            "title": " Advanced Certificate in Machine Learning & Data Science",
+            "issuer": "IIT Madras",
+            "description": "Comprehensive certification covering advanced machine learning concepts, algorithms, and practical applications.",
+            "pdf_path": "/static/assets/certificates/Advance Certificate in ML.pdf",
+            "date": "2025"
+        },
+        {
+            "title": " Advanced Certificate in Programming & Application Development ",
+            "issuer": "IIT Madras",
+            "description": "Comprehensive certification covering advanced programming concepts, algorithms, and practical applications.",
+            "pdf_path": "/static/assets/certificates/Advance Certificate in Programming.pdf",
+            "date": "2025"
+        }
+    ]
